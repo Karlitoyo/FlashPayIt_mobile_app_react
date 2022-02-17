ad924e19f6178f5b3e31531c76196269ea64e2ff
karl2017 <dismothaphucka@gmail.com>
2022-02-17 18:11:45 +0000

    Amended tables on user_login to customer table + created business role table + campaign role table

--- server/migrations/3_add_business_and_campaign_table.sql
+++ server/migrations/3_add_business_and_campaign_table.sql
@@ -1,28 +1,78 @@
+ALTER TABLE users_login (
+    ADD id INT GENERATED ALWAYS AS IDENTITY,
+    ADD phone_number INT not null,
+    ADD name text not null,
+    ADD description text,
+    ADD start_date date,
+    ADD end_date date,
+    ADD active bool,
+    ADD created_at timestampz,
+    ADD updated_at timestampz,
+);
+
+CREATE TABLE business(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    vat_number varchar(256) not null,
+    contact INT not null,
+    active bool,
+);
+
 CREATE TABLE campaign (
-    customer_id varchar(256),
-    campaign_id varchar(256),
-    name varchar(256),
-    description varchar(256),
-    start_date varchar(256),
-    end_date varchar(256),
-    active varchar(256),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    customer_id INT,
+    business_id INT,
+    name text not null,
+    description text not null,
+    start_date date,
+    end_date date,
+    active bool not null,
     created_at timestampz,
     updated_at timestampz,
+    CONSTRAINT fk_business
+    FOREIGN KEY(business_id)
+    REFERENCES business(id),
+    CONSTRAINT fk_customer
+    FOREIGN KEY(customer_id)
+    REFERENCES users_login(id)
+);
+
+ALTER TABLE users_login RENAME TO customer;
+
+CREATE TABLE business_role(
+    id text,
+);
+
+CREATE TABLE campaign_role(
+    id text,
 );
 
 CREATE TABLE business_login (
-    business_id varchar(256),
-    email varchar(256),
-    username varchar(256),
-    contact varchar(256),
-    vat_number varchar(256),
-    active varchar(256),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    business_id INT,
+    email text not null,
+    username text not null,
+    role text,
     created_at timestampz,
     updated_at timestampz,
+    CONSTRAINT fk_business
+    FOREIGN KEY(business_id)
+    REFERENCES business(id),
+    CONSTRAINT fk_business_role
+    FOREIGN KEY(role)
+    REFERENCES business_role(id)
 );
 
-CREATE TABLE business()
-CREATE TABLE business_user(business_id, user_id, role)
-CREATE TABLE business_campaign()
-CREATE TABLE business_campaign_user()
-CREATE TABLE role()
+CREATE TABLE campaign_user(
+    role text,
+    campaign_id INT,
+    customer_id INT,
+    CONSTRAINT fk_customer
+    FOREIGN KEY(customer_id)
+    REFERENCES customer(id),
+    CONSTRAINT fk_campaign
+    FOREIGN KEY(campaign_id)
+    REFERENCES campaign(id),
+    CONSTRAINT fk_campaign_role
+    FOREIGN KEY(role)
+    REFERENCES campaign_role(id)
+);
